67cc3882b0a998a7bc9526268f3a5f365a077c44
hyewon-han <hw1635@hanmail.net>
2023-10-26 19:34:13 +0900

    모달창 생성

---
+++ js/darkmode.js
@@ -0,0 +1,7 @@
+//다크모드
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+darkModeToggle.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
+//다크모드
--- js/review.js
+++ js/review.js
@@ -5,13 +5,14 @@ const reviewStar = document.querySelector(".review__star");
 const reviewTextarea = document.querySelector(".review__textarea");
 const reviewSubmitBtn = document.querySelector(".review__submitbtn");
 const reviewLists = document.getElementById("review__lists");
+
 const URLSearch = new URLSearchParams(location.search);
 let id = parseInt(URLSearch.get("id"));
 
-let reviews = [];
+let reviewStorage = [];
 
 function saveReview() {
-  localStorage.setItem("review", JSON.stringify(reviews));
+  localStorage.setItem("review", JSON.stringify(reviewStorage));
 }
 
 function submitReview(e) {
@@ -26,7 +27,7 @@ function submitReview(e) {
     movieid: parseInt(id),
   };
 
-  reviews.push(reviewObj);
+  reviewStorage.push(reviewObj);
   reviewForm.reset();
 
   paintReview(reviewObj);
@@ -38,6 +39,7 @@ reviewForm.addEventListener("submit", submitReview);
 
 function paintReview(reviewObj) {
   const li = document.createElement("li");
+  const btn = document.createElement("button");
   li.id = reviewObj.id;
   li.classList.add("review__list");
   li.innerHTML = `<div>
@@ -47,9 +49,28 @@ function paintReview(reviewObj) {
 <p>
   ${reviewObj.review}
 </p>
-<div>
-  <button>삭제</button>
-</div>`;
+            <div class="hidden modal__container">
+              <div class="modal__content">
+                <input
+                  type="password"
+                  placeholder="비밀번호를 입력하세요"
+                  required
+                />
+                <div>
+                  <button class="modal__close-btn" class="btn">취소</button>
+                  <button class="modal__delete-btn" class="btn">삭제</button>
+                </div>
+              </div>
+            </div>
+`;
+  btn.innerText = "모달";
+  btn.classList.add("modal__open-btn");
+  btn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
+  // btn.addEventListener("click", deleteReview);
+
+  li.appendChild(btn);
   reviewLists.append(li);
 }
 
@@ -58,16 +79,39 @@ const saveReviews = localStorage.getItem("review");
 if (saveReviews !== null) {
   //만약 Reviews가 localStorage에 존재하면 실행할 코드
   const parsedReviews = JSON.parse(saveReviews);
-  reviews = parsedReviews;
-  console.log(parsedReviews);
+  reviewStorage = parsedReviews;
   parsedReviews.filter((obj) => obj.movieid == id).forEach(paintReview);
 }
 
 function handleTextareaEnter(e) {
   if (e.key == "Enter") {
-    console.log(e);
     reviewSubmitBtn.click();
   }
 }
 
 reviewTextarea.addEventListener("keyup", handleTextareaEnter);
+
+function deleteReview(event) {
+  const li = event.target.parentElement;
+  reviewStorage = reviewStorage.filter(
+    (review) => review.id !== parseInt(li.id)
+  );
+  saveReview();
+  li.remove();
+}
+
+//    modal
+// const modalOpenButton = document.querySelector(".modal__open-btn");
+const modalCloseButton = document.querySelectorAll(".modal__close-btn");
+const modalDeleteBtn = document.querySelector(".modal__delete-btn");
+const modal = document.querySelector(".modal__container");
+
+// modalOpenButton.addEventListener("click", () => {
+//   modal.classList.remove("hidden");
+// });
+
+modalCloseButton.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+// modalDeleteBtn.forEach((btn) => btn.addEventListener("click", deleteReview));
