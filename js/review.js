c64d8028189538af4dbc0a42a738cd1cf86f4383
hyewon-han <hw1635@hanmail.net>
2023-10-26 11:05:14 +0900

    review작성 기능 구현

--- js/review.js
+++ js/review.js
@@ -5,15 +5,17 @@ const reviewStar = document.querySelector(".review__star");
 const reviewTextarea = document.querySelector(".review__textarea");
 const reviewSubmitBtn = document.querySelector(".review__submitbtn");
 const reviewLists = document.getElementById("review__lists");
+const URLSearch = new URLSearchParams(location.search);
+let id = parseInt(URLSearch.get("id"));
+
 let reviews = [];
-// let id = storage.length;
+
+function saveReview() {
+  localStorage.setItem("review", JSON.stringify(reviews));
+}
 
 function submitReview(e) {
   e.preventDefault();
-  console.log(reviewUsername.value);
-  console.log(reviewPassword.value);
-  console.log(reviewStar.value);
-  console.log(reviewTextarea.value);
 
   const reviewObj = {
     name: reviewUsername.value,
@@ -21,13 +23,14 @@ function submitReview(e) {
     star: reviewStar.value,
     review: reviewTextarea.value,
     id: Date.now(),
+    movieid: parseInt(id),
   };
 
   reviews.push(reviewObj);
-  localStorage.setItem("review", JSON.stringify(storage));
   reviewForm.reset();
 
   paintReview(reviewObj);
+  saveReview();
 }
 
 reviewForm.addEventListener("submit", submitReview);
@@ -36,5 +39,26 @@ reviewForm.addEventListener("submit", submitReview);
 function paintReview(reviewObj) {
   const li = document.createElement("li");
   li.id = reviewObj.id;
-  li.innerHTML = ``;
+  li.classList.add("review__list");
+  li.innerHTML = `<div>
+  <span>${reviewObj.name}</span>
+  <span>${reviewObj.star}</span>
+</div>
+<p>
+  ${reviewObj.review}
+</p>
+<div>
+  <button>삭제</button>
+</div>`;
+  reviewLists.append(li);
+}
+
+const saveReviews = localStorage.getItem("review");
+
+if (saveReviews !== null) {
+  //만약 Reviews가 localStorage에 존재하면 실행할 코드
+  const parsedReviews = JSON.parse(saveReviews);
+  reviews = parsedReviews;
+  console.log(parsedReviews);
+  parsedReviews.filter((obj) => obj.movieid == id).forEach(paintReview);
 }
