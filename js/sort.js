f70fc7a81f618d47a5370b0c82d3eb9a17879d0f
coldbrew <minsuk815@naver.com>
2023-10-26 19:34:32 +0900

    상세페이지css수정

---
+++ js/sort.js
@@ -0,0 +1,17 @@
+
+
+
+// export const sortMovie = function () {
+//     let unsortmovies = document.querySelectorAll('.movie-card__vote-average')
+//     unsortmovies = Array.from(unsortmovies)
+
+//     console.log(unsortmovies[0].textContent.slice(10, 14))
+
+// }
+export const sortMovie = function () {
+    let unsortmovies = document.querySelectorAll('.movie-card__vote-average')
+    unsortmovies = Array.from(unsortmovies)
+
+    console.log(unsortmovies[0].textContent.slice(10, 14))
+
+}
