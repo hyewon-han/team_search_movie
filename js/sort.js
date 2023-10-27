98ef7c461edbf057e79a561998752a51f020cbaf
Dello96 <enadios03@gmail.com>
2023-10-27 10:04:53 +0900

    sort

--- js/sort.js
+++ js/sort.js
@@ -1,6 +1,3 @@
-
-
-
 // export const sortMovie = function () {
 //     let unsortmovies = document.querySelectorAll('.movie-card__vote-average')
 //     unsortmovies = Array.from(unsortmovies)
@@ -8,10 +5,23 @@
 //     console.log(unsortmovies[0].textContent.slice(10, 14))
 
 // }
-export const sortMovie = function () {
-    let unsortmovies = document.querySelectorAll('.movie-card__vote-average')
-    unsortmovies = Array.from(unsortmovies)
 
-    console.log(unsortmovies[0].textContent.slice(10, 14))
+const averageBtn = document.querySelector(".ratingBtn");
 
+export function sortMovie () {
+    const sortMovies = document.querySelectorAll('.movie-card__vote-average');
+
+    function handleSort (e) {
+        e.preventDefault();
+        const sortedMovieList = [];
+
+        sortMovies.forEach((sortMovies, card) => {
+            const avrg = sortMovies;
+            sortedMovieList.push(sortMovies.innerHTML.substring(10));
+            sortedMovieList.sort((a, b) => b - a);
+        })
+
+        console.log(sortedMovieList);
+    }
+    averageBtn.addEventListener("click", handleSort);
 }
